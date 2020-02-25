2a3bdd276400d8494b7a1b18df4f50f6599c6cea
sidi-64ers <sidichahid@gmail.com>
2020-02-25 14:35:04 +0100

    first commit

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;        
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail("toto@lolo.fr");
+        $hashedPsd = $this->passwordEncoder->encodePassword($user, "toto");
+        $user->setPassword($hashedPsd);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
+
